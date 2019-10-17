0e28c0da21b0067be21276f1369d5c938e74fae7
MOHIT <moit16ee@cmrit.ac.in>
2019-10-17 15:53:34 +0530

    Updated tree

--- Tree/Binarytree.py
+++ Tree/Binarytree.py
@@ -1,14 +1,63 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __str__(self):
+        s = ""
+        for i in range(len(self.items)):
+            s += str(self.items[i].value) + "-"
+        return s
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def __len__(self):
+        return self.size()
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
 class Node(object):
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-
-
 class BinaryTree(object):
     def __init__(self, root):
         self.root = Node(root)
-
     def print_tree(self, traversal_type):
         if traversal_type == "preorder":
             return self.preorder_print(tree.root, "")
@@ -20,7 +69,6 @@ class BinaryTree(object):
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
             return False
-
     def preorder_print(self, start, traversal):
         """Root->Left->Right"""
         if start:
@@ -28,7 +76,6 @@ class BinaryTree(object):
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
         return traversal
-
     def inorder_print(self, start, traversal):
         """Left->Root->Right"""
         if start:
@@ -36,7 +83,6 @@ class BinaryTree(object):
             traversal += (str(start.value) + "-")
             traversal = self.inorder_print(start.right, traversal)
         return traversal
-
     def postorder_print(self, start, traversal):
         """Left->Right->Root"""
         if start:
@@ -44,6 +90,31 @@ class BinaryTree(object):
             traversal = self.postorder_print(start.right, traversal)
             traversal += (str(start.value) + "-")
         return traversal
+    def height(self, node):
+        if node is None:
+            return -1
+        left_height=self.height(node.left)
+        right_height=self.height(node.right)
+        return 1 + max(left_height, right_height)
+    def size(self):
+        if self.root is None:
+            return 0
+        st=Stack()
+        st.push(self.root)
+        size=1
+        while st:
+            node=st.pop()
+            if node.left:
+                size+=1
+                st.push(node.left)
+            if node.right:
+                size+=1
+                st.push(node.right)
+        return size
+    def size_recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self.size_recursive(node.left) + self.size_recursive(node.right)
 #               1
 #           /       \  
 #          2          3  
@@ -57,3 +128,5 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
+print(tree.size())
+print(tree.size_recursive(tree.root))
